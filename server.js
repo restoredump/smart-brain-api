c1270b3fad5239cca134ba925a01731b345bb5bb
beforethunderr <dewaputraja34@gmail.com>
2020-07-24 04:36:21 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/Register',(req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', image.handleimage(db))
+app.post('/imageurl', image.handleApiCall)
+
+
+app.listen(3000, () => {
+   console.log('app is running');
+})
+
+// *
+// --> res = this is working
+// signin --> Post = success/fail
+// register --> Post = user
+// profile/:userId --> GET = user
+// image --> PUT --> user
+// complete
