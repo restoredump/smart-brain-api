56591c6338cf145da33746329049b2766b0302ff
beforethunderr <dewaputraja34@gmail.com>
2020-07-24 08:30:13 +0700

    CAPEK

--- server.js
+++ server.js
@@ -10,34 +10,25 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
-    }
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
 });
 
 const app = express();
 
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('it is working') })
+app.get('/', (req, res)=> { res.send('it is bitch work') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-
-app.listen(process.env.PORT || 3000, () => {
-   console.log(`app is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port 3000 ${process.env.PORT}`);
 })
-
-// *
-// --> res = this is working
-// signin --> Post = success/fail
-// register --> Post = user
-// profile/:userId --> GET = user
-// image --> PUT --> user
-// complete
