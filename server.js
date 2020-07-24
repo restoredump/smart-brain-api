038eea05df6b681007fe9730848d9b9b6b6ae9d6
beforethunderr <dewaputraja34@gmail.com>
2020-07-25 01:14:33 +0700

    pleaseeeeeeeeeee

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
-  client: 'pg',
+const db =  knex({
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -26,7 +25,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> { res.send('it is bitch work') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', (req, res) => { signin.hangleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
