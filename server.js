7042201f2607b7304e4ce0eb8713516a8ba98e59
beforethunderr <dewaputraja34@gmail.com>
2020-07-24 08:07:02 +0700

    fix v2

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { res.send('it is working') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register',(req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', image.handleimage(db))
-app.post('/imageurl', image.handleApiCall)
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
 app.listen(process.env.PORT || 3000, () => {
