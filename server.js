c18f6674ee273241f438fc14256b59c7e80b26c2
beforethunderr <dewaputraja34@gmail.com>
2020-07-25 22:10:02 +0700

    pleaseeeee

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> { res.send('it is work') })
-app.post('/signin', (req, res)=> { signin.hangleSignin(req, res, db, bcrypt) })
+app.post('/signin', (req, res)=> { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
