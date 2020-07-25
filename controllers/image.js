5180db493ae02b1689d5ef7c33de62b0442343da
beforethunderr <dewaputraja34@gmail.com>
2020-07-25 19:17:29 +0700

    idk

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: '1634d988f51141129995e0f4fbdec90d'
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
